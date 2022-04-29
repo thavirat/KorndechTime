7fdbbbdb005796fb3eab4651446c02c01beed9ae
SriQ <mobile.korndech@gmail.com>
2022-04-29 17:03:45 +0700

    แก้บั๊ก

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -71,9 +71,36 @@ class HomeController extends Controller
             $error_msg = curl_error($curl);
         }
         
-        
+        echo 'นำเข้ารายชื่อ '.$response.' รายการ';
         curl_close($curl);
-        dd($response);
+        
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => env('API_URL').'TransferDataTimeWork',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS =>'{
+            "branch": "'.env('BRANCH_ID').'"
+        }',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.env('AUTH_TOKEN'),
+            'Content-Type: application/json'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        echo '<br>';
+        echo 'ประมวลผลสำเร็จ '.$response.' รายการ';
     }
 
     /**
